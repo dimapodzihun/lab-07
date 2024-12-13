acd3f1197d3d2fc633d8b91948deccfb3d861e0e
Asus <Asus@DESKTOP-PNDABF0>
2024-12-13 14:49:22 +0200

    Add project files.

---
+++ lab 07/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox3.Text = "Не введено данних!";
+                return;
+            }
+            double x1 = Convert.ToDouble(textBox1.Text);
+            double x2 = Convert.ToDouble(textBox2.Text);
+            double y = ((Math.Sqrt(Math.Pow(x1, 3) + Math.Pow(x2, 5))) / 1000) + 65;
+            textBox3.Text = y.ToString("0.####");
+            double average = (x1 + x2) / 2;
+            textBox4.Text = average.ToString("0.####");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text=string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+
+        }
+    }
+}
